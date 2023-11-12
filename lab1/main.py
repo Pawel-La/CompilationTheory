8d14d68b091c4215ea7620aa5ac6d85abb6e785c
Paweł Lamża <103361216+Pawel-La@users.noreply.github.com>
2023-11-12 13:13:35 +0100

    added drawing of ast to main

--- lab1/main.py
+++ lab1/main.py
@@ -1,16 +1,27 @@
 import sys
 import scanner
+import parser
+from ast_draw import draw_ast
+
+EXAMPLES_PATH = "examples"
+
 
 if __name__ == '__main__':
 
     try:
-        filename = sys.argv[1] if len(sys.argv) > 1 else "example.txt"
+        filename = sys.argv[1] \
+            if len(sys.argv) > 1 \
+            else f"{EXAMPLES_PATH}//example4.txt"
         file = open(filename, "r")
+        text = file.read()
     except IOError:
         print("Cannot open {0} file".format(filename))
         sys.exit(0)
 
-    text = file.read()
-    lexer = scanner.Scanner()
-    for tok in lexer.tokenize(text):
-        print('(%r): %s(%s)' % (tok.lineno, tok.type, tok.value))
+    lexer = scanner.MyLexer()
+    parser = parser.MyParser()
+
+    result = parser.parse(lexer.tokenize(text))
+    if result:
+        draw_ast(result)
+
