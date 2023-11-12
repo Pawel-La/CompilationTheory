ee5a1edca81f17801533f9e6783ebb2537a01bd1
Paweł Lamża <103361216+Pawel-La@users.noreply.github.com>
2023-11-12 13:12:41 +0100

    added building of AST and also commented redundant precedence

--- lab1/parser.py
+++ lab1/parser.py
@@ -9,7 +9,7 @@ class MyParser(Parser):
     precedence = (
         ("nonassoc", IF_PREC),
         ("nonassoc", ELSE),
-        ('nonassoc', "=", INC_ASSIGN, DEC_ASSIGN, MUL_ASSIGN, DIV_ASSIGN),
+        # ('nonassoc', "=", INC_ASSIGN, DEC_ASSIGN, MUL_ASSIGN, DIV_ASSIGN)
         ('nonassoc', EQ, NOT_EQ),
         ('nonassoc', "<", ">", GREATER_EQ, LESS_EQ),
         ('left', "+", "-", DOT_PLUS, DOT_MINUS),
@@ -18,12 +18,13 @@ class MyParser(Parser):
         ('left', "\'")
     )
 
-    @_(
-        "Statement StatementList",
-        "Statement"
-    )
+    @_("Statement StatementList")
     def StatementList(self, p):
-        pass
+        return [p[0]] + p[1]
+
+    @_("Statement")
+    def StatementList(self, p):
+        return [p[0]]
 
     @_(
         "CompoundStatement",
@@ -36,52 +37,52 @@ class MyParser(Parser):
         'Expression ";"',
     )
     def Statement(self, p):
-        pass
+        return p[0]
 
     @_('"{" StatementList "}"')
     def CompoundStatement(self, p):
-        pass
+        return p[1]
 
-    # had to defined IF_PREC here because it didn't define itself automatically
-    @_(
-        'IF "(" Expression ")" Statement %prec IF_PREC',
-        'IF "(" Expression ")" Statement ELSE Statement',
-    )
+    @_('IF "(" Expression ")" Statement %prec IF_PREC')
     def IfStatement(self, p):
-        pass
+        return "IF_TAG", (("IF", p[2]), ("THEN", p[4]))
+
+    @_('IF "(" Expression ")" Statement ELSE Statement')
+    def IfStatement(self, p):
+        return "IF_ELSE_TAG", (("IF", p[2]), ("THEN", p[4]), ("ELSE", p[6]))
 
     @_('WHILE "(" Expression ")" Statement')
     def WhileStatement(self, p):
-        pass
+        return 'WHILE', p[2], p[4]
 
     @_(
         'FOR ID "=" Range Statement',
         'FOR ID "=" List Statement',
     )
     def ForStatement(self, p):
-        pass
+        return 'FOR', p[1], p[3], p[4]
 
-    @_(
-        "BREAK",
-        "CONTINUE",
-        "RETURN Expression",
-    )
+    @_("BREAK", "CONTINUE")
     def JumpStatement(self, p):
-        pass
+        return p[0].upper()
+
+    @_("RETURN Expression")
+    def JumpStatement(self, p):
+        return p[0], p[1]
 
     @_("PRINT ListContent")
     def PrintStatement(self, p):
-        pass
+        return 'PRINT', p[1]
 
     @_(
-        'LeftValue "="         Expression',
-        "LeftValue INC_ASSIGN  Expression",
-        "LeftValue DEC_ASSIGN  Expression",
-        "LeftValue DIV_ASSIGN  Expression",
-        "LeftValue MUL_ASSIGN  Expression",
+        'ID_Content "="         Expression',
+        "ID_Content INC_ASSIGN  Expression",
+        "ID_Content DEC_ASSIGN  Expression",
+        "ID_Content DIV_ASSIGN  Expression",
+        "ID_Content MUL_ASSIGN  Expression",
     )
     def AssignmentStatement(self, p):
-        pass
+        return (p[1], p[0], p[2])
 
     @_(
         'Expression "+"         Expression',
@@ -98,66 +99,94 @@ class MyParser(Parser):
         'Expression LESS_EQ     Expression',
         'Expression ">"         Expression',
         'Expression "<"         Expression',
-        '"(" Expression ")"',
-        '"-" Expression %prec UMINUS',
-        'Expression "\'"',
+    )
+    def Expression(self, p):
+        return (p[1], p[0], p[2])
+
+    @_('"(" Expression ")"')
+    def Expression(self, p):
+        return p[1]
+
+    @_('"-" Expression %prec UMINUS')
+    def Expression(self, p):
+        return ('-', p[1])
+
+    @_('Expression "\'"')
+    def Expression(self, p):
+        return ('TRANSPOSE', p[0])
+
+    @_(
         "List",
         "Number",
         "STRING",
-        "LeftValue",
-        "MatrixFunction",
+        "ID_Content",
+        "MatrixSpecialFunction",
     )
     def Expression(self, p):
-        pass
+        return p[0]
 
     @_('RangeElement ":" RangeElement')
     def Range(self, p):
-        pass
+        return ("RANGE", p[0], p[2])
 
     @_(
         "Number",
         "ID"
     )
     def RangeElement(self, p):
-        pass
+        return p[0]
 
     @_('"[" ListContent "]"')
     def List(self, p):
-        pass
+        return 'LIST', p[1]
 
-    @_(
-        'Expression "," ListContent',
-        "Expression",
-    )
+    @_('Expression "," ListContent')
     def ListContent(self, p):
-        pass
+        return [p[0]] + p[2]
 
-    @_(
-        "ID",
-        "ID List",
-    )
-    def LeftValue(self, p):
-        pass
+    @_("Expression")
+    def ListContent(self, p):
+        return [p[0]]
+
+    @_("ID")
+    def ID_Content(self, p):
+        return p[0]
+
+    @_("ID ListAccess")
+    def ID_Content(self, p):
+        return 'ACCESS', p[0], p[1]
+
+    @_('"[" ListAccessElement "]"')
+    def ListAccess(self, p):
+        return ([p[1]])
+
+    @_('"[" ListAccessElement "," ListAccessElement "]"')
+    def ListAccess(self, p):
+        return ([p[1]] + [p[3]])
+
+    @_('DECIMAL', 'ID')
+    def ListAccessElement(self, p):
+        return p[0]
 
     @_(
         'EYE "(" Expression ")"',
         'ZEROS "(" Expression ")"',
         'ONES "(" Expression ")"',
     )
-    def MatrixFunction(self, p):
-        pass
+    def MatrixSpecialFunction(self, p):
+        return (p[0].upper(), p[2])
 
     @_(
         "DECIMAL",
         "FLOAT"
     )
     def Number(self, p):
-        pass
+        return p[0]
 
     def error(self, p):
-        if not p:
-            print("End of File!")
-        else:
+        if p:
             print("Syntax error at line {0}: LexToken({1}, '{2}')".format(
                 p.lineno, p.type, p.value)
             )
+        else:
+            print("End of File!")
