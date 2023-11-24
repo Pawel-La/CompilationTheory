091ee57d6b13e69516a873d67ab18c0840ca39ca
Kujonick <przemek.r@opoczta.pl>
2023-11-24 18:48:54 +0100

    slightly changed main

--- lab1/main.py
+++ lab1/main.py
@@ -1,9 +1,9 @@
 import sys
 import scanner
-import parser
+import Parser
 from ast_draw import draw_ast
 
-EXAMPLES_PATH = "examples"
+EXAMPLES_PATH = "lab1/examples"
 
 
 if __name__ == '__main__':
@@ -11,7 +11,7 @@ if __name__ == '__main__':
     try:
         filename = sys.argv[1] \
             if len(sys.argv) > 1 \
-            else f"{EXAMPLES_PATH}//example4.txt"
+            else f"{EXAMPLES_PATH}/example4.txt"
         file = open(filename, "r")
         text = file.read()
     except IOError:
@@ -19,7 +19,7 @@ if __name__ == '__main__':
         sys.exit(0)
 
     lexer = scanner.MyLexer()
-    parser = parser.MyParser()
+    parser = Parser.MyParser()
 
     result = parser.parse(lexer.tokenize(text))
     if result:
